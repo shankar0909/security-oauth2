e8e8bf472897be765f97ac9fd994cb1868b39254
shankar09-personal <shankar09-personal@shankars-mbp-2.lan>
2020-02-23 16:09:40 -0500

    Initial Commit

---
+++ authentication-cognito-oauth-demo/src/main/java/com/personal/security/authenticationcognitooauthdemo/AuthenticationCognitoOauthDemoApplication.java
@@ -0,0 +1,13 @@
+package com.personal.security.authenticationcognitooauthdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationCognitoOauthDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationCognitoOauthDemoApplication.class, args);
+	}
+
+}
---
+++ authentication-cognito-oauth-demo/src/test/java/com/personal/security/authenticationcognitooauthdemo/AuthenticationCognitoOauthDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.security.authenticationcognitooauthdemo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationCognitoOauthDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
